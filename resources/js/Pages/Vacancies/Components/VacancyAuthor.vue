5fa3f5105f2e7b9e2e6af91dab1f2f9471ef1b1d
DemianWHT <demiankost2@gmail.com>
2024-02-19 09:47:36 +0100

    Added author info on show page

--- resources/js/Pages/Vacancies/Components/VacancyAuthor.vue
+++ resources/js/Pages/Vacancies/Components/VacancyAuthor.vue
@@ -1,7 +1,19 @@
 <template>
-    
+    <div class="vacancy__author px-4 py-2">
+        <div class="flex flex-col items-center">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png" class="w-20 h-20 rounded-full object-cover mb-4" />
+            <h2 class="text-lg font-semibold">{{ author.name }}</h2>
+        </div>
+    </div>
 </template>
 
 <script setup>
-    
+    import { defineProps } from 'vue';
+
+    let props = defineProps({
+        author: {
+            type: Object,
+            required: true
+        }
+    });
 </script>
--- resources/js/Pages/Vacancies/Show.vue
+++ resources/js/Pages/Vacancies/Show.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container mx-auto py-10 mt-14 flex gap-x-4">
-        <div class="w-8/12 px-4 pr-10 border-r border-gray-200">
+        <div class="w-9/12 px-4 pr-10 border-r border-gray-200">
             <h1 class="text-4xl font-bold mb-4">{{ vacancy.data.title }}</h1>
             <div v-if="vacancy.data.tags" class="categories mb-4 flex items-center gap-x-3 gap-y-4">
                 <span v-for="tag in vacancy.data.tags" :key="`tag_${tag.name}`" class="text-sm bg-gray-200 px-3 py-1 rounded-full">{{ tag.name }}</span>
@@ -9,14 +9,17 @@
                 {{ vacancy.data.description }}
             </div>
         </div>
-        <div class="w-4/12 px-4">
-
+        <div class="w-3/12 px-4">
+            <VacancyAuthor
+                :author="vacancy.data.author"
+            />
         </div>
     </div>
 </template>
 
 <script setup>
     import { defineProps } from 'vue';
+    import VacancyAuthor from './Components/VacancyAuthor.vue';
 
     defineProps({
         vacancy: {
