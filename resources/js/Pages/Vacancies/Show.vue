ee2fe4c0ddf9690b957e03ff92199e86bd47950f
DemianWHT <demiankost2@gmail.com>
2024-02-19 09:30:03 +0100

    Fixed showing categories on vacancy single page

--- resources/js/Pages/Vacancies/Show.vue
+++ resources/js/Pages/Vacancies/Show.vue
@@ -2,8 +2,8 @@
     <div class="container mx-auto py-10 mt-14 flex gap-x-4">
         <div class="w-8/12 px-4 pr-10 border-r border-gray-200">
             <h1 class="text-4xl font-bold mb-4">{{ vacancy.data.title }}</h1>
-            <div v-if="vacancy.tags" class="categories mb-4 flex items-center gap-x-3 gap-y-4">
-                <span v-for="tag in vacancy.tags" :key="`tag_${tag.name}`" class="text-sm bg-gray-200 px-3 py-1 rounded-full">{{ tag.name }}</span>
+            <div v-if="vacancy.data.tags" class="categories mb-4 flex items-center gap-x-3 gap-y-4">
+                <span v-for="tag in vacancy.data.tags" :key="`tag_${tag.name}`" class="text-sm bg-gray-200 px-3 py-1 rounded-full">{{ tag.name }}</span>
             </div>
             <div class="content">
                 {{ vacancy.data.description }}
